dcd3a40c2caddb0e540392bae90ecf5e5d8f5ecf
Blanca Jimenez <blanca.jimenezvicente@gmail.com>
2025-06-18 13:30:05 +0200

    fix utilities init

--- library/utilities/__init__.py
+++ library/utilities/__init__.py
@@ -1,4 +1,4 @@
-from .read_image_numpy import read_image_numpy
+from .read_image_numpy import read_image_numpy, read_tiff_image
 from .save_model import save_model
 from .load_model import load_model
 from .print_directory_structure import print_directory_structure
